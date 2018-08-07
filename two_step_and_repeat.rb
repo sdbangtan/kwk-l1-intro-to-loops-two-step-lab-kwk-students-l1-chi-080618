9469f30cc03d33390a0f34ea8b51add236b090b1
Saumya Malhotra <ideagem1@gmail.com>
2018-08-07 21:26:01 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,6 +1,10 @@
 def first_steps
   # Write a loop that outputs "Right foot back", "Left foot back",
   # "Right foot back", and "Stop", with sleep after each
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
 end
 
 def a_few_more_steps
